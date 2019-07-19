140658a72e3b37ff9d2699eba6f16622d48c269c
stoffee <chris@roadkill.org>
2019-07-19 13:22:43 -0700

    adding outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "key_vault_name" {
   value = "${azurerm_key_vault.vault.name}"
 }
 
-output "vault_public_ip" {
-  value = "${azurerm_public_ip.vault.id}"
-}
+#output "vault_public_ip" {
+#  value = "${azure_lb.vault.public_ip_address_id}"
+#}
